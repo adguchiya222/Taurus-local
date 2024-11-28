46fa22c54aac1282a9a9ae1f4da06d7f3a8ce528
adnan <adguchiya222@gmail.com>
2024-11-28 16:12:16 +0300

    New Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import DashboardLayout from './DashboardLayout';
+
 import AppProviderTheme from './AppProviderTheme';
 
 function App() {
   return (
     <div>
      
-      {/* <DashboardLayout /> */}
+      
       <AppProviderTheme/>
     </div>
   );
